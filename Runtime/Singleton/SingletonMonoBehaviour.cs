798fc0285bc04d9fbf1eab284b3b24c4d7185672
Stefan <stefan.produktivkeller@gmail.com>
2023-12-15 16:01:48 +0100

    [0.1.5] Add support for disabling 'Domain Reload' to 'SingletonMonoBehaviour'.

--- Runtime/Singleton/SingletonMonoBehaviour.cs
+++ Runtime/Singleton/SingletonMonoBehaviour.cs
@@ -5,6 +5,27 @@ namespace Produktivkeller.SimpleCodePatterns.Singleton
     public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : Component
     {
         public static T Instance { get; private set; }
+        
+        /**
+         * Reset the instance field even if 'Domain Reload' is disabled in the player settings.
+         *
+         * https://docs.unity3d.com/Manual/DomainReloading.html
+         */
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        static void ResetEvenWithoutDomainReload()
+        {
+            if (!Instance)
+            {
+                return;
+            }
+
+            SingletonMonoBehaviour<T> singletonMonoBehaviour = Instance as SingletonMonoBehaviour<T>;
+            
+            if (singletonMonoBehaviour && singletonMonoBehaviour.ShouldResetEvenWithoutDomainReload())
+            {
+                Instance = null;
+            }
+        }
 
         protected virtual void Initialize()
         {
@@ -13,6 +34,11 @@ namespace Produktivkeller.SimpleCodePatterns.Singleton
         protected virtual bool ShouldDestroyOnLoad()
         {
             return false;
+        }
+
+        protected virtual bool ShouldResetEvenWithoutDomainReload()
+        {
+            return true;
         }
 
         public virtual void Awake()
